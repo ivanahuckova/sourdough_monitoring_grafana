547e6b417f0ab868942a68110b793be93460b49b
Ivana Huckova <30407135+ivanahuckova@users.noreply.github.com>
2022-03-15 15:23:58 +0100

    Update config.h

--- config.h
+++ config.h
@@ -5,11 +5,11 @@
 #define ID "sourdough" // Add unique name for this sensor
 #define INTERVAL 5 //seconds
 
-#define DHTPIN 27    // Which pin is DHT 11 connected to
+#define DHTPIN 32    // Which pin is DHT 11 connected to
 #define DHTTYPE DHT11 // Type DHT 11
 
-#define ULTRASONIC_PIN_TRIG 2 // Which pin is HC-SR04's trig connected txo
-#define ULTRASONIC_PIN_ECHO 4 // Which pin is HC-SR04's echo connected to
+#define ULTRASONIC_PIN_TRIG 4 // Which pin is HC-SR04's trig connected txo
+#define ULTRASONIC_PIN_ECHO 5 // Which pin is HC-SR04's echo connected to
 
 #define GC_PORT 443
 #define GC_PROM_URL "prometheus-us-central1.grafana.net"
